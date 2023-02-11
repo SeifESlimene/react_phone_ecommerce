4150844a6123083d04b0f972a1eeb8e1baa428f6
seifeslimene <s.slimene19@gmail.com>
2023-02-11 16:39:47 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/cart";
+import Default from "./components/Default";
+import Modal from "./components/Modal";
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList}/>
+        <Route path="/details" component={Details}/>
+        <Route path="/cart" component={Cart}/>
+        <Route component={Default}></Route>
+      </Switch>
+      <Modal />
+    </React.Fragment>
+  );
+}
+
+export default App;
